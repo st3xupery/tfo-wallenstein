9ae680755803bc66b8c40d43b2e72f10caa438d8
Daniel A <danielalksnis@gmail.com>
2017-03-22 19:43:28 -0400

    Added more dates and efo and obhecc properties

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13)
 
+var colorCode = {
+	tfo: 'red',
+	efo: 'green',
+	obhecc: 'yellow'
+}
+
 var tfoGeoJson = {
 	type: 'FeatureCollection',
 	features: tfo.map(x => ({
 		type: 'Feature',
+		properties: {association: 'tfo'},
 		geometry: x
-	}))
-}
-
-var tfoGeoJsonPoints = {
-	type: 'FeatureCollection',
-	features: tfo.map(x => ({
-		type: 'Feature',
-		geometry: {
-			type: 'Point',
-			coordinates: x.coordinates[0][0]
-		}
-	}))
+	})).concat(efoObhecc)
 }
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -27,20 +23,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 	id: 'mapbox.streets'
 }).addTo(mymap);
 
-var myIcon = new L.icon({
-    iconUrl: 'map-marker.png',
-    iconSize: [24, 24],
-    iconAnchor: [12, 12],
-});
-
 var _tfoGeoJson = L.geoJSON(tfoGeoJson, {
-	style: feature => ({color: 'black'})
-}).addTo(mymap);
-
-L.geoJSON(tfoGeoJsonPoints, {
-	pointToLayer: function(feature, latlng) {
-		return L.marker(latlng, {icon: myIcon})
-	}
+	style: feature => ({color: colorCode[feature.properties.association], weight: 3})
 }).addTo(mymap);
 
 const eqIdToColor = {};
@@ -48,16 +32,28 @@ const eqIdToColor = {};
 L.geoJSON(ls, {
 	style: feature => {
 		const color = eqIdToColor[feature.properties.equipmentId] = `#${Math.floor(Math.random()*16777215).toString(16)}`
-		return { color, weight: 1, dashArray: '10, 3, 2, 3' }
+		return { color, weight: 1, dashArray: '10, 3, 2, 3', opacity: 0.5 }
 	}
 }).bindPopup(function (layer) {
     return layer.feature.properties.equipmentId;
 }).addTo(mymap);
 
-L.geoJSON(pt, {
+
+const walGPS = L.geoJSON(pt, {
 	pointToLayer: function(feature, latlng) {
 		return L.circleMarker(latlng, {radius: 1, color: eqIdToColor[feature.properties.equipmentId]})
 	}
 }).addTo(mymap);
+
+mymap.on('zoomend', function () {
+	currentZoom = mymap.getZoom();
+	if (currentZoom > 15) {
+        walGPS.setStyle({weight: 10});
+	} else if (currentZoom > 10) {
+        walGPS.setStyle({weight: 5});
+	} else {
+        walGPS.setStyle({weight: 1});
+    }
+});
 
 mymap.fitBounds(_tfoGeoJson.getBounds(), {animate: true, padding: [10, 10]});
